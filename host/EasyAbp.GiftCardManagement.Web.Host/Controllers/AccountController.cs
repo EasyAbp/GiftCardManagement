5d3c10a0f4b612e3b26b9431cb2d894d56546a88
gdlcf88 <47396430@qq.com>
2020-04-04 02:40:01 +0800

    Delete host folder

--- host/EasyAbp.GiftCardManagement.Web.Host/Controllers/AccountController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.AspNetCore.Mvc.Authentication;
-
-namespace EasyAbp.GiftCardManagement.Controllers
-{
-    public class AccountController : ChallengeAccountController
-    {
-
-    }
-}
